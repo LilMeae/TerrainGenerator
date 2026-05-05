5464945d00f93046f7d52e70c63714646e1ebd66
Hongyi Sun <kevin09.sun@gmail.com>
2026-05-05 10:48:34 -0400

    Fixed the maven dependencies

---
+++ src/main/java/com/project/ui/MainApp.java
@@ -0,0 +1,17 @@
+package com.project.ui;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start
+}
